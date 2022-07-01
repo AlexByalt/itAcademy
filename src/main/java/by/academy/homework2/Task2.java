924cdf964838eec7c5050c44e36731778b2fe740
AlexByalt <alex_byalt@mail.ru>
2022-07-01 14:00:32 +0300

    1

--- src/main/java/by/academy/homework2/Task2.java
+++ src/main/java/by/academy/homework2/Task2.java
@@ -6,6 +6,10 @@ public class Task2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n;
+        int size = 0;
+        int min = 0;
+        int index = 0;
+        boolean b = true;
         System.out.println("Введите колличество слов");
         if (sc.hasNextInt()) {
             n = sc.nextInt();
@@ -13,18 +17,38 @@ public class Task2 {
             System.out.println("Ошибка!");
             return;
         }
-        String[] str = new String[n];
+        String[] words = new String[n];
         sc.nextLine();
         for (int i = 0; i < n; i++) {
             System.out.println("Введите слово №" + (i + 1));
-            str[i] = sc.nextLine();
+            words[i] = sc.nextLine();
         }
-        for (int j = 0; j < str.length; j++) {
-            for (int k = j + 1; k < str.length; j++) {
-                if (str[k] == str[j]) {
-                    System.out.println("число различных элементов равно " + k);
+        for (int j = 0; j < words.length; j++) {
+            String word = words[j];
+            char[] letters = new char[word.length()];
+            for (int k = 0; k < word.length(); k++) {
+                if (k == 0) {
+                    letters[k] = word.charAt(k);
+                    size++;
+                } else {
+                    for (int l = 0; l < letters.length; l++) {
+                        if (letters[l] == word.charAt(j)) {
+                            b = false;
+                        }
+                    }
+                    if (b == true) {
+                        letters[j] = word.charAt(j);
+                        size++;
+                    }
                 }
+                b = true;
             }
+            if (min == 0 || min > size) {
+                min = size;
+                index = j;
+            }
+            size = 0;
         }
+        System.out.println(words[index]);
     }
 }
