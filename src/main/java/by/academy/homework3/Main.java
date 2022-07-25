a9db10ee4fd9ef442bba53eb9c3daee600625e28
AlexByalt <alex_byalt@mail.ru>
2022-07-25 19:02:24 +0300

    1

--- src/main/java/by/academy/homework3/Main.java
+++ src/main/java/by/academy/homework3/Main.java
@@ -1,6 +1,10 @@
 package by.academy.homework3;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 //NOT READY YET, NEED SOME TIME!!!
+// THERE ARE A LOF OF BUGS(
 public class Main {
     public static void main(String[] args) {
         Motorbike motorbike1 = new Motorbike("Motorbike", 10500, 2, 21000, "Suzuki", 1.2, 2005);
@@ -23,6 +27,110 @@ public class Main {
         productList[6] = truck1;
         productList[7] = truck2;
         productList[8] = truck3;
+        User userOne = new User();
+        User userTwo = new User();
+        Deal deal = new Deal();
+        deal.setBuyer(userOne);
+        deal.setSeller(userTwo);
 
+        int choise = -1;
+        boolean tip = false;
+        String answer = "0";
+
+        Scanner sc = new Scanner(System.in);
+        ;
+        do {
+            System.out.println("0: menu");
+            System.out.println("+: add");
+            System.out.println("-: remove");
+            answer = sc.next();
+            switch (answer) {
+                case "+": {
+                    System.out.println("menu:");
+                    for (int i = 0; i < productList.length; i++) {
+                        System.out.println(i + 1 + ": " + productList[i].getProdName() + "-" + productList[i].getPrice() + " BYN");
+                    }
+                    System.out.println("0: menu");
+                    do {
+                        tip = false;
+                        System.out.println("choose your product:");
+                        if (sc.hasNextInt()) {
+                            choise = sc.nextInt();
+
+                            if (choise == 0) {
+                                break;
+                            }
+                            if (choise < 1 || choise > productList.length) {
+                                System.out.println("error");
+                                tip = true;
+                                continue;
+                            }
+
+                            for (int i = 0; i < deal.getProducts().length; i++) {
+                                if (deal.getProduct(i) == productList[choise - 1] && deal.getProducts().length > 0) {
+                                    tip = true;
+                                    System.out.println("you've already added this product");
+                                    break;
+                                }
+                            }
+
+                            if (tip == true) {
+                                continue;
+                            }
+
+                            System.out.println("enter quantity");
+                            if (sc.hasNextDouble()) {
+                                double q = sc.nextDouble();
+                                deal.addProduct(productList[choise - 1]);
+                                productList[choise - 1].setQuantity(q);
+                                break;
+                            }
+                        }
+                    } while (choise >= 1 && choise <= productList.length || tip == true);
+                    break;
+                }
+                case "-": {
+                    System.out.println("menu");
+                    do {
+                        tip = false;
+                        System.out.println("choose your product");
+                        if (sc.hasNextInt()) {
+                            choise = sc.nextInt();
+
+                            if (choise == 0) {
+                                break;
+                            }
+                            if (choise < 1 || choise > productList.length) {
+                                System.out.println("error");
+                                tip = true;
+                                continue;
+                            }
+
+                            for (int i = 0; i < deal.getProducts().length; i++) {
+                                if (deal.getProduct(i) == productList[choise - 1] && deal.getProducts().length > 0) {
+                                    deal.removeProduct(productList[choise - 1]);
+                                    productList[choise - 1].setQuantity(0);
+                                    tip = false;
+                                    break;
+                                }
+                            }
+                        } else {
+                            System.out.println("error");
+                        }
+                    } while (tip == true);
+                    break;
+                }
+                case "0": {
+                    System.out.println("good choise!");
+                    break;
+                }
+                default: {
+                    System.out.println("error");
+                    answer = "continue";
+                }
+            }
+        } while (answer.equals("+") || answer.equals("-") || answer.equals("continue"));
+        System.out.println(Arrays.toString(deal.getProducts()));
+        sc.close();
     }
 }
